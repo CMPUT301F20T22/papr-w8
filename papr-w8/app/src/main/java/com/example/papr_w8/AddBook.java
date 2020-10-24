77e2f2cfaf4688ea1bf09c08b3aeba4ed61f3361
Muhammad Mazhar Hussain <hussain2@ualberta.ca>
2020-10-23 22:28:02 -0600

    added add book activity + others
    -added button drawables
    -added addbook.java and add book activity
    -some other shit

---
+++ papr-w8/app/src/main/java/com/example/papr_w8/AddBook.java
@@ -0,0 +1,12 @@
+package com.example.papr_w8;
+
+import android.widget.Button;
+
+public class AddBook {
+
+    private Books book;
+    private Button cancel;
+    private Button confirm_add;
+
+    
+}
