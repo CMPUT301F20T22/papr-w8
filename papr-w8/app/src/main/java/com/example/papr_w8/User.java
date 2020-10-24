a163feb68d4134bfc3d19005124c24838ed84900
WenrrrBeth <bethding0923@gmail.com>
2020-10-24 15:58:23 -0600

    add array of books in user class

--- papr-w8/app/src/main/java/com/example/papr_w8/User.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/User.java
@@ -1,16 +1,20 @@
 package com.example.papr_w8;
 
+import java.util.ArrayList;
+
 public class User {
     private String name;
     private String contact;
     private String email;
     private String address;
+    private ArrayList<Books> books;
 
     public User(String name, String contact, String email, String address){
         this.name = name;
         this.contact = contact;
         this.email = email;
         this.address = address;
+        this.books = new ArrayList<Books>();
     }
 
     public String getName() {
@@ -43,5 +47,13 @@ public class User {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public ArrayList<Books> getBooks() {
+        return books;
+    }
+
+    public void addBooks(Books book) {
+        books.add(book);
     }
 }
