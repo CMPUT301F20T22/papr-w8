9e6e641399f5e6769292c2321177a0cfd2a2b059
ScottKavalinas <36358126+SalvationStalk@users.noreply.github.com>
2020-10-28 01:58:06 -0600

    updated books class

--- papr-w8/app/src/main/java/com/example/papr_w8/Book.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/Book.java
@@ -2,19 +2,27 @@ package com.example.papr_w8;
 
 import java.io.Serializable;
 
-public class Book implements Serializable {
+
+public class Book {
     private String title;
     private String author;
-    private String ISBN;
+    private String isbn;
+    private String description;
     private String status;
+    private String photo; // Using string as placeholder only
 
-    public Book(String title, String author, String ISBN, String status) {
+    private String location; // make Location class that has doubles for long/ Lat???
+
+    public Book(String title, String author, String isbn, String description,String status, String photo, String location) {
         this.title = title;
         this.author = author;
-        this.ISBN = ISBN;
+        this.isbn = isbn;
+        this.description = description;
         this.status = status;
+        this.photo = photo;//this must be made optional
+        this.location = location;
     }
-
+    //title
     public String getTitle() {
         return title;
     }
@@ -22,7 +30,7 @@ public class Book implements Serializable {
     public void setTitle(String title) {
         this.title = title;
     }
-
+    //author
     public String getAuthor() {
         return author;
     }
@@ -30,15 +38,23 @@ public class Book implements Serializable {
     public void setAuthor(String author) {
         this.author = author;
     }
-
+    //isbn
     public String getISBN() {
-        return ISBN;
+        return isbn;
     }
 
-    public void setISBN(String ISBN) {
-        this.ISBN = ISBN;
+    public void setISBN(String isbn) {
+        this.isbn = isbn;
+    }
+    //description
+    public String getDescription() {
+        return description;
     }
 
+    public void setDescription(String isbn) {
+        this.description = description;
+    }
+    //status
     public String getStatus() {
         return status;
     }
@@ -46,4 +62,21 @@ public class Book implements Serializable {
     public void setStatus(String status) {
         this.status = status;
     }
+    //photo
+    public String getPhoto() {
+        return status;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    //location
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 }
