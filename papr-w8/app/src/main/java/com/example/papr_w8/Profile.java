6fdfe13302c187c475b10cc0b67837799383d898
WenrrrBeth <bethding0923@gmail.com>
2020-10-28 02:57:44 -0600

    profile version fixed

--- papr-w8/app/src/main/java/com/example/papr_w8/Profile.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/Profile.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavGraph;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,32 +24,25 @@ import android.widget.TextView;
 public class Profile extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private static final String USER = "user";
-
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     private User user;
 
-//    public Profile(User user){
-//        this.user = user;
-//    }
-
     public Profile() {
         // Required empty public constructor
     }
-
+    
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param param1
+     * @param param2
      * @return A new instance of fragment Profile.
      */
     // TODO: Rename and change types and number of parameters
@@ -68,34 +62,34 @@ public class Profile extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         TextView name = (TextView)view.findViewById(R.id.name);
-        TextView contact = (TextView)view.findViewById(R.id.number);
+//        TextView contact = (TextView)view.findViewById(R.id.number);
         TextView email = (TextView)view.findViewById(R.id.email);
         TextView address = (TextView)view.findViewById(R.id.address);
         Button edit = (Button)view.findViewById(R.id.EditProfile);
 
-        name.setText(user.getName());
-        contact.setText(user.getContact());
-        email.setText(user.getEmail());
-        address.setText(user.getAddress());
+        if(user != null) {
+            name.setText(user.getName());
+//            contact.setText(user.getContact());
+            email.setText(user.getEmail());
+            address.setText(user.getAddress());
 
-        edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), EditProfile.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-            }
-        });
+            edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getActivity(), EditProfile.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                }
+            });
+        }
         // Inflate the layout for this fragment
         return view;
     }
---
+++ papr-w8/app/src/main/java/com/example/papr_w8/SignUpActivity.java
@@ -0,0 +1,128 @@
+package com.example.papr_w8;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+    private EditText usernameET;
+    private EditText passwordET;
+    private EditText emailET;
+    private EditText addressET;
+    private FirebaseAuth firebaseAuth;
+    private Button sign_up_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        usernameET = findViewById(R.id.editTextUserName);
+        passwordET = findViewById(R.id.editTextPassword);
+        emailET = findViewById(R.id.editTextEmailAddress);
+        addressET = findViewById(R.id.editTextTextPostalAddress);
+
+        sign_up_button = findViewById(R.id.sign_up_button);
+
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = usernameET.getText().toString();
+                final String email = emailET.getText().toString();
+                final String password = passwordET.getText().toString();
+                final String address = addressET.getText().toString();
+
+                if (TextUtils.isEmpty(username)) {
+                    usernameET.setError("Invalid username.");
+                    usernameET.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    passwordET.setError("Invalid password.");
+                    passwordET.requestFocus();
+                    return;
+                }
+                if (password.length() < 6){
+                    passwordET.setError("Password must be at least 6 characters.");
+                    passwordET.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    emailET.setError("Invalid email.");
+                    emailET.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    emailET.setError("Please provide valid email.");
+                    emailET.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(address)) {
+                    addressET.setError("Invalid address.");
+                    addressET.requestFocus();
+                    return;
+                }
+                // check username doesn't already exist
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(username, password, email, address);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        //redirect to success page
+                                    } else {
+                                        Toast.makeText(SignUpActivity.this, "Unable to sign up.", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                        }
+                        else{
+                            Toast.makeText(SignUpActivity.this, "Failed to sign up.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+
+                });
+
+
+            }
+
+            ;
+        });
+    }
+}
