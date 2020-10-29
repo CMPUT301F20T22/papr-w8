107a81a05304d32cffdc0c652cdd32292f71aa49
ScottKavalinas <36358126+SalvationStalk@users.noreply.github.com>
2020-10-28 22:18:35 -0600

    updated books owned with floating button

--- papr-w8/app/src/main/java/com/example/papr_w8/BooksOwned.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/BooksOwned.java
@@ -2,13 +2,34 @@ package com.example.papr_w8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BooksOwned extends AppCompatActivity {
 
+    public BooksOwned (){
+
+    }
+   private FloatingActionButton floating_add_book;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books_owned);
+
+        floating_add_book = findViewById(R.id.add_book_float);
+        floating_add_book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddBook();
+            }
+        });
+    }
+    public void openAddBook(){
+        Intent intent = new Intent(this, AddBook.class);
+        startActivity(intent);
     }
 }
