57653600bc4b1faef6a6d0466e3faf6da9b3a124
Muhammad Mazhar Hussain <hussain2@ualberta.ca>
2020-10-29 17:31:20 -0600

    added firebase implementation but does not work properly so must be fixed

--- papr-w8/app/src/main/java/com/example/papr_w8/AddBook.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/AddBook.java
@@ -2,15 +2,29 @@ package com.example.papr_w8;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddBook extends AppCompatActivity {
 
     private Book book;
     private Bitmap newCover;
+    public static final String TAG = "TAG";
+
 //    private EditText newBookTitle;
 //    private EditText newISBNTitle;
 //    private Button cancel;
@@ -28,6 +42,11 @@ public class AddBook extends AppCompatActivity {
         final EditText newBookAuthor = findViewById(R.id.new_author_editText);
         Button cancel = findViewById(R.id.cancel_addbook_button);
         Button confirm = findViewById(R.id.confirm_addbook_button);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore fbDB = FirebaseFirestore.getInstance();
+        String uid = firebaseAuth.getCurrentUser().getUid();
+        final DocumentReference db = fbDB.collection("Books").document(uid);
 
 
         confirm.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +72,27 @@ public class AddBook extends AppCompatActivity {
                     return;
                 } else {
 //                    new Book(title, author, ISBN);
+                    Map<String, Object> book = new HashMap<>();
+                    book.put("Title", title);
+                    book.put("Author", author);
+                    book.put("ISBN", ISBN);
+
+                    // Firebase implementation does not work properly:
+                    db.collection("Books")
+                            .add(book)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
+                    finish();
                 }
             }
         });
