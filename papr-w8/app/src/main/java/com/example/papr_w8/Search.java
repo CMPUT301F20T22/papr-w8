c966c9e62df18d2497fc863733f5100b8f379541
mxiuwang <mxwang@ualberta.ca>
2020-10-30 16:25:30 -0600

    search switch pages on spinner selection

--- papr-w8/app/src/main/java/com/example/papr_w8/Search.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/Search.java
@@ -33,46 +33,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
  */
 public class Search extends Fragment implements AdapterView.OnItemSelectedListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Search() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Search.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Search newInstance(String param1, String param2) {
-        Search fragment = new Search();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -116,6 +76,29 @@ public class Search extends Fragment implements AdapterView.OnItemSelectedListen
             }
         });
 
+        // get emails from firebase
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final DocumentReference userDoc = FirebaseFirestore.getInstance().collection("Users").document(user.getEmail());
+
+        userDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+            if (task.isSuccessful()){
+                DocumentSnapshot doc = task.getResult();
+                if (doc.exists()) {
+                    // display user profiles
+
+                    Log.d("Sample", "DocumentSnapshot data: " + doc.getData());
+                } else { // if user profile does not exist
+                    // display empty listview
+                    Log.d("Sample", "No such user");
+                }
+            } else { // if unable to retrive from firebase
+                Log.d("Sample", "get failed with ", task.getException());
+            }
+            }
+        });
+
         // Inflate the layout for this fragment
         return view;
     }
@@ -138,25 +121,3 @@ public class Search extends Fragment implements AdapterView.OnItemSelectedListen
 
     }
 }
-
-//        // get emails from firebase
-//        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//        final DocumentReference userDoc = FirebaseFirestore.getInstance().collection("Users").document(user.getEmail());
-//
-//        userDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//            if (task.isSuccessful()){
-//                DocumentSnapshot doc = task.getResult();
-//                if (doc.exists()) {
-//                    // display user profiles in listview
-//                    Log.d("Sample", "DocumentSnapshot data: " + doc.getData());
-//                } else { // if user profile does not exist
-//                    // display empty listview
-//                    Log.d("Sample", "No such user");
-//                }
-//            } else { // if unable to retrive from firebase
-//                Log.d("Sample", "get failed with ", task.getException());
-//            }
-//            }
-//        });
