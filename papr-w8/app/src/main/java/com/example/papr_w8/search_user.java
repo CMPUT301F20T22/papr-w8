2c7f42aee867c3f06434c72ea30fcac54e958395
mxiuwang <mxwang@ualberta.ca>
2020-10-30 02:09:38 -0600

    search spinner goes to different pages

---
+++ papr-w8/app/src/main/java/com/example/papr_w8/search_user.java
@@ -0,0 +1,24 @@
+package com.example.papr_w8;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link search_user#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class search_user extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_user, container, false);
+    }
+}
