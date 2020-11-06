f6f0a63be36ad1f992deaacf762d9aef5ec20a65
WenrrrBeth <bethding0923@gmail.com>
2020-11-06 15:39:40 -0700

    profile test done

---
+++ papr-w8/app/src/androidTest/java/com/example/papr_w8/ProfileTest.java
@@ -0,0 +1,61 @@
+package com.example.papr_w8;
+
+import android.app.Fragment;
+import android.widget.EditText;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.navigation.Navigation;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProfileTest{
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<MainActivity>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+        solo.enterText((EditText) solo.getView((R.id.email)), "test@mail.com");
+        solo.enterText((EditText) solo.getView(R.id.password), "something");
+        solo.clickOnButton("Login");
+        solo.waitForFragmentById(R.id.fragment);
+        solo.clickOnImage(3);
+    }
+
+
+    /**
+     * check if everything is presented correctly on profile page
+     */
+    @Test
+    public void checkInfo(){
+        solo.waitForFragmentById(R.id.profile);
+        solo.waitForText("test");
+        solo.waitForText("test@mail.com");
+        solo.waitForText("1234");
+        solo.clickOnButton("Edit Profile");
+        solo.assertCurrentActivity("Wrong Activity", EditProfile.class);
+    }
+
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+
+}
--- papr-w8/app/src/main/java/com/example/papr_w8/Profile.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/Profile.java
@@ -26,56 +26,14 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link Profile#newInstance} factory method to
- * create an instance of this fragment.
+ * profile fragment after user click the profile bottom navigation button
  */
 public class Profile extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private User currentUser;
-    private Context context;
 
     public Profile() {
         // Required empty public constructor
     }
 
-
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Profile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Profile newInstance(String param1, String param2) {
-        Profile fragment = new Profile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -87,6 +45,8 @@ public class Profile extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView name = (TextView)view.findViewById(R.id.name);
 //        TextView contact = (TextView)view.findViewById(R.id.number);
+
+        // get all the TextView
         final TextView email = (TextView)view.findViewById(R.id.email);
         final TextView address = (TextView)view.findViewById(R.id.address);
         Button edit = (Button)view.findViewById(R.id.EditProfile);
