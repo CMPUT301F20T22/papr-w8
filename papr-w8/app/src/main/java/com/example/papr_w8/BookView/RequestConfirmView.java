3655cf08acdd8666980a1fdc0a136b00ec6bfecb
fredford <fredford@ualberta.ca>
2020-11-25 19:18:19 -0700

    updated UserRequestsDisplayList to pass a book to RequestConfirmView
    updated UserRequestsDisplayList to pass a book to RequestConfirmView

--- papr-w8/app/src/main/java/com/example/papr_w8/BookView/RequestConfirmView.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/BookView/RequestConfirmView.java
@@ -3,14 +3,24 @@ package com.example.papr_w8.BookView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.papr_w8.Book;
 import com.example.papr_w8.R;
 
 public class RequestConfirmView extends AppCompatActivity {
 
+    public RequestConfirmView(){
+
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_confirm_view);
+
+        final Book book = (Book) getIntent().getSerializableExtra("book");
+
+        Log.d("hello", book.getOwner());
     }
 }
