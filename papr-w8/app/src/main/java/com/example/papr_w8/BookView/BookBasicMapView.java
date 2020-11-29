1ca19d1b84e2558c6c95fb48d950744e08033f7a
WenrrrBeth <bethding0923@gmail.com>
2020-11-28 21:33:10 -0700

    Request Confirm View done

--- papr-w8/app/src/main/java/com/example/papr_w8/BookView/BookBasicMapView.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/BookView/BookBasicMapView.java
@@ -1,5 +1,7 @@
 package com.example.papr_w8.BookView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,20 +12,38 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.papr_w8.Book;
 import com.example.papr_w8.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * This is a Fragment that displays the view of a basic Book Description
  */
-public class BookBasicMapView extends BookBase {
+public class BookBasicMapView extends BookBase implements OnMapReadyCallback {
+
+    GoogleMap map;
+    Book book;
 
     @Override
     public void onCreate( Bundle savedInstanceState ){
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
+        // Get the bundle containing the Book object passed to the View
+        Bundle bundle = this.getArguments();
+        book = (Book) bundle.getSerializable("bookSelected");
+
     }
 
     @Override
@@ -33,6 +53,17 @@ public class BookBasicMapView extends BookBase {
         ViewStub stub = baseView.findViewById(R.id.child_fragment_here);
         stub.setLayoutResource(R.layout.fragment_book_basic_map);
         stub.inflate();
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     };
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng bookLoc = book.getLocation();
+        map.addMarker(new MarkerOptions().position(bookLoc).title("book location"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(bookLoc));
+    }
 }
