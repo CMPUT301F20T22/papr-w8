8e2694dfeb6781732fbc18720dd12fb6d8fcff96
Melanie Zuniga <mzuniga@ualberta.ca>
2020-11-28 19:42:15 -0700

    added mapview to bookcheckoutview

--- papr-w8/app/src/main/java/com/example/papr_w8/Adapters/UserRequestsDisplayList.java
+++ papr-w8/app/src/main/java/com/example/papr_w8/Adapters/UserRequestsDisplayList.java
@@ -1,9 +1,9 @@
+
 package com.example.papr_w8.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,8 +50,7 @@ public class UserRequestsDisplayList extends ArrayAdapter<User> {
             view = LayoutInflater.from(context).inflate(R.layout.user_requests_display_list_item,parent,false);
         }
 
-        User user = users.get(position);
-        Log.d("BOOK ID", book.getId());
+        final User user = users.get(position);
 
         TextView userName = (TextView) view.findViewById(R.id.user_name_text);
         Button acceptRequest = (Button) view.findViewById(R.id.accept_request);
@@ -64,6 +63,7 @@ public class UserRequestsDisplayList extends ArrayAdapter<User> {
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), RequestConfirmView.class);
                 intent.putExtra("book", book);
+                intent.putExtra("borrower", user.getEmail());
                 context.startActivity(intent);
             }
         });
