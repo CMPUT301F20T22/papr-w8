90a3e2234909359fdcd9c753005099c1e4f4466e
fredford <fredford@ualberta.ca>
2020-11-29 22:28:06 -0700

    Update BookReturningViewTest.java

--- papr-w8/app/src/androidTest/java/com/example/papr_w8/BookView/BookReturningViewTest.java
+++ papr-w8/app/src/androidTest/java/com/example/papr_w8/BookView/BookReturningViewTest.java
@@ -1,5 +1,6 @@
 package com.example.papr_w8.BookView;
 
+import android.app.Activity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,12 +11,15 @@ import androidx.test.rule.ActivityTestRule;
 import com.example.papr_w8.Host;
 import com.example.papr_w8.MainActivity;
 import com.example.papr_w8.R;
+import com.example.papr_w8.ScanActivity;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookReturningViewTest {
 
@@ -48,53 +52,73 @@ public class BookReturningViewTest {
 
         // Go to books owned fragment
         solo.clickOnButton("Books Borrowed");
+
+        solo.clickInList(0);
     }
 
+    /**
+     * Check that the current fragment is for Book Returning
+     * @throws Exception
+     */
     @Test
     public void checkFragment() throws Exception {
 
-        solo.clickInList(0);
         solo.waitForFragmentById(R.id.book_returning);
-
 
     }
 
+    /**
+     * Check that the information displayed is accurate and in the correct location
+     * @throws Exception
+     */
     @Test
     public void checkInformation() throws Exception {
 
-        //final TextView titleView = solo.getView(R.)
+        final TextView titleView = (TextView) solo.getView(R.id.titleEditText);
+        final TextView authorView = (TextView) solo.getView(R.id.authorEditText);
+        final TextView isbnView = (TextView) solo.getView(R.id.isbnEditText);
+        final TextView statusView = (TextView) solo.getView(R.id.statusEditText);
+        final TextView ownerView = (TextView) solo.getView(R.id.ownerEditText);
 
-        solo.waitForText("A Clockwork Orange");
-        solo.waitForText("Anthony Burgess");
-        solo.waitForText("Borrowed");
-        solo.waitForText("9780393341768");
-        solo.waitForText("mazi@mail.com");
+        assertEquals("A Clockwork Orange", titleView.getText().toString());
+        assertEquals("Anthony Burgess", authorView.getText().toString());
+        assertEquals("Borrowed", statusView.getText().toString());
+        assertEquals("9780393341768", isbnView.getText().toString());
+        assertEquals("mazi@mail.com", ownerView.getText().toString());
 
     }
 
 
-
+    /**
+     * Check that the cancel button returns the fragment to the host page
+     * @throws Exception
+     */
     @Test
     public void checkCancel() throws Exception {
 
         solo.clickOnButton("Cancel");
-
-
-
+        solo.waitForFragmentById(R.id.fragment);
 
     }
 
+    /**
+     * Check that the return button opens the scan activity
+     * @throws Exception
+     */
     @Test
     public void checkReturn() throws Exception {
 
-        solo.clickOnButton("Books Borrowed");
-        solo.clickInList(0);
         solo.clickOnButton("Return Book");
 
+        Activity activity = rule.getActivity();
+        solo.assertCurrentActivity("Wrong Activity", ScanActivity.class);
 
     }
 
-
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
     @After
     public void tearDown() throws Exception {
         solo.finishOpenedActivities();
