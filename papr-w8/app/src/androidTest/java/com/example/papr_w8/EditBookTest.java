387d171dcb6956b52f819de988c8ce994283dae8
Muhammad Mazhar Hussain <hussain2@ualberta.ca>
2020-11-29 22:05:02 -0700

    DONE edit book test

--- papr-w8/app/src/androidTest/java/com/example/papr_w8/EditBookTest.java
+++ papr-w8/app/src/androidTest/java/com/example/papr_w8/EditBookTest.java
@@ -1,12 +1,10 @@
 package com.example.papr_w8;
 
-import android.view.View;
 import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
-import com.example.papr_w8.AddPack.AddBook;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -44,95 +42,52 @@ public class EditBookTest {
         solo.getText("Books Owned", false);
 
         //add book to edit
+        solo.clickOnView(solo.getView(R.id.add_book_float));
+        solo.enterText((EditText) solo.getView(R.id.new_title_editText), "Edit Title");
+        solo.enterText((EditText) solo.getView(R.id.new_author_editText), "Edit Author");
+        solo.enterText((EditText) solo.getView(R.id.new_isbn_editText), "TEST12345678");
         solo.clickOnButton("Add Book");
-        View fab = getActivity().findViewById(R.id.myFloatingActionButton);
-        solo.clickOnView(fab);
+        assertTrue(solo.waitForText("Book Added", 1, 10000));
+        //return to shelves after adding a book
     }
 
 
-//    /**
-//     * Checks if edittexts being empty works before adding a book
-//     */
-//    @Test
-//    public void checkEmpty() {
-//        solo.assertCurrentActivity("Wrong Activity", EditBook.class);
-//
-//        solo.clickOnButton("Add Book");
-//        assertTrue(solo.waitForText("Please enter title", 1, 2000));
-//
-//        solo.enterText((EditText) solo.getView(R.id.new_title_editText), "Test Title");
-//        solo.clickOnButton("Add Book");
-//        assertTrue(solo.waitForText("Please enter author's name", 1, 2000));
-//
-//
-//        solo.enterText((EditText) solo.getView(R.id.new_author_editText), "Test Author");
-//        solo.clickOnButton("Add Book");
-//        assertTrue(solo.waitForText("Please enter valid ISBN", 1, 2000));
-//    }
-
     /**
-     * Checks if books are added when 'Add Book' is clicked
+     * Checks if books are edited
      */
     @Test
     public void checkEdit() {
-
-        solo.assertCurrentActivity("Wrong Activity", AddBook.class);
-
-        solo.enterText((EditText) solo.getView(R.id.new_title_editText), "Test Add Title");
-        solo.enterText((EditText) solo.getView(R.id.new_author_editText), "Test Add Author");
-        solo.enterText((EditText) solo.getView(R.id.new_isbn_editText), "TEST12345678");
-
-        solo.clickOnButton("Add Book");
-        assertTrue(solo.waitForText("Book Added", 1, 10000));
-
+        // checks if current activity is the shelves page
         solo.assertCurrentActivity("Wrong Activity", Host.class);
 
+        // switch to books owned page and check if book added it there
         solo.clickOnButton("Books Owned");
-        assertTrue(solo.waitForText("Test Add Title", 1, 5000));
-        assertTrue(solo.waitForText("Test Add Author", 1, 5000));
+        assertTrue(solo.waitForText("Edit Title", 1, 5000));
+        assertTrue(solo.waitForText("Edit Author", 1, 5000));
         assertTrue(solo.waitForText("TEST12345678", 1, 5000));
 
-
-
-
-//        solo.assertCurrentActivity("Wrong Activity", AddBook.class);
-//        solo.clickOnButton("Cancel");
-//
-        solo.clickOnButton("Books Owned");
+        // click on the first book and edit
         solo.clickInList(0);
-
-
-//        solo.clickLongOnText("Edit Test");
-//        assertTrue(solo.waitForText("Edit Test", 1, 2000));
-//        assertTrue(solo.waitForText("Author Edit", 1, 2000));
-//        assertTrue(solo.waitForText("TEST12345678", 1, 2000));
         solo.clickOnButton("Edit Description");
-
+        // clear editTexts
+        solo.clearEditText((EditText) solo.getView(R.id.edit_title_editText));
+        solo.clearEditText((EditText) solo.getView(R.id.edit_author_editText));
+        solo.clearEditText((EditText) solo.getView(R.id.edit_isbn_editText));
+        //input edited book desc
         solo.enterText((EditText) solo.getView(R.id.edit_title_editText), "Edited Title Test");
         solo.enterText((EditText) solo.getView(R.id.edit_author_editText), "Edited Author Test");
         solo.enterText((EditText) solo.getView(R.id.edit_isbn_editText), "EDITED12345678");
         solo.clickOnButton("Confirm");
         assertTrue(solo.waitForText("Book Edited", 1, 10000));
-
+        // after editing book should go back to shelves page
         solo.assertCurrentActivity("Wrong Activity", Host.class);
-
+        // go back to Books Owned and check if book has been edited
         solo.clickOnButton("Books Owned");
         assertTrue(solo.waitForText("Edited Title Test", 1, 2000));
         assertTrue(solo.waitForText("Edited Author Test", 1, 2000));
         assertTrue(solo.waitForText("EDITED12345678", 1, 2000));
-
     }
 
-//    /**
-//     * Check if File Chooser opens
-//     */
-//    @Test
-//    public void checkFileChooser() {
-//        solo.assertCurrentActivity("Wrong Activity", AddBook.class);
-//        ImageButton imageButton = (ImageButton) solo.getView("imageButton");
-//        solo.clickOnView(imageButton);
-//        solo.assertCurrentActivity("Wrong Activity", AddBookCoverActivity.class);
-//    }
 
     /**
      * Closes activites after tests
